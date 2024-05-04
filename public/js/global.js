773aa929e725b4ca57d7709198c9898f957b4bd1
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-05-04 17:49:11 +0800

    task007-切換黑暗模式

--- public/js/global.js
+++ public/js/global.js
@@ -1,31 +1,52 @@
 const home = document.getElementById('home')
 const darkMode = document.getElementById('dark-mode')
+const theme = document.getElementById('theme')
 
 init()
 
 function init() {
-  home.addEventListener('click', backToHome)
-  darkMode.addEventListener('click', toggleDarkMode)
+  // local storage 預設為light模式
+  setTheme()
+  home.addEventListener('click', onRedirectHome)
+  darkMode.addEventListener('click', onToggleMode)
 }
 
-function backToHome(event) {
-  const target = event.target
-  if (target.id === 'home' || target.tagName === 'IMG') {
-    const currentPath = window.location.href
-    const fileName = currentPath.split('/').slice(-1)[0]
-    if (fileName === 'home.html') {
-      window.location.href = currentPath
-    } else {
-      window.location.href = '../home/home.html'
-    }
+// #監聽器函式: Home鍵
+function onRedirectHome() {
+  const currentPath = window.location.href
+  const fileName = currentPath.split('/').slice(-1)[0]
+  if (fileName === 'home.html') {
+    window.location.href = currentPath
+  } else {
+    window.location.href = '../home/home.html'
   }
 }
 
-function toggleDarkMode(event) {
-  const target = event.target
+// #監聽器函式: 黑暗模式切換
+function onToggleMode() {
+  toggleMode()
+  if (storage.get('theme') === 'light') {
+    storage.set('theme', 'dark')
+  } else if (storage.get('theme') === 'dark') {
+    storage.set('theme', 'light')
+  }
+}
+
+// 其他函式: 黑暗模式圖示切換
+function toggleMode() {
   const icon = darkMode.children[0]
-  if (target.id === 'dark-mode' || target.tagName === 'I') {
-    icon.classList.toggle('fa-sun')
-    icon.classList.toggle('fa-moon')
+  // toggle icon
+  icon.classList.toggle('fa-sun')
+  icon.classList.toggle('fa-moon')
+  // toggle theme
+  theme.classList.toggle('dark')
+}
+
+// 其他函式: 黑暗模式切換(local storage)
+function setTheme() {
+  if (!storage.get('theme')) {
+    storage.set('theme', 'light')
+  } else if (storage.get('theme') === 'dark') {
+    toggleMode()
   }
 }
