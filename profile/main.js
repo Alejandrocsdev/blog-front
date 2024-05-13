03491aa4085a2e49e9c95151fd29cc9444b953e0
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-05-13 11:40:09 +0800

    upload image

--- profile/main.js
+++ profile/main.js
@@ -4,14 +4,15 @@ const USER_API = `${BASE_URL}/users`
 // HTML元素
 const username = document.getElementById('username')
 const email = document.getElementById('email')
-const avatar = document.querySelector('#avatar img')
+const avatarForm = document.getElementById('avatar-form')
+const avatarImg = document.querySelector('#avatar-form img')
+const fileInput = document.getElementById('fileInput')
 const userArticles = document.getElementById('user-articles')
 
 const articles = []
 
 token = cookie.get('token') || ''
 user = cookie.get('user') || ''
-
 ;(function init() {
   // 從cookie移除article_id
   cookie.remove('article_id')
@@ -21,8 +22,46 @@ user = cookie.get('user') || ''
   userArticlesRequest()
   // 監聽器: 文章標題導向edit頁面
   userArticles.addEventListener('click', onTitleRedirect)
+  // 監聽器: 點擊照片
+  avatarImg.addEventListener('click', onClickAvatar)
+  // 監聽器: 上傳照片
+  fileInput.addEventListener('change', onUpload)
+  // 監聽器: 提交表單
+  avatarForm.addEventListener('submit', onSubmit)
 })()
 
+// 監聽器: 點擊照片
+function onClickAvatar() {
+  fileInput.click()
+}
+
+// 監聽器: 上傳照片
+function onUpload() {
+  const file = this.files[0]
+  if (file) {
+    const reader = new FileReader()
+    // 監聽器: 更新上傳照片
+    reader.onload = (event) => (avatarImg.src = event.target.result)
+    // 觸發onload事件
+    reader.readAsDataURL(file)
+    onSubmit()
+  }
+}
+
+// 監聽器: 提交表單
+function onSubmit(event) {
+  if (event) {
+    event.preventDefault();
+  }
+  const file = fileInput.files[0]
+  if (file) {
+    const formData = new FormData()
+    formData.append('file', file)
+    uploadImageRequest(formData)
+  }
+}
+
+// 請求會員文章
 function userArticlesRequest() {
   axios
     .get(`${USER_API}/${user.id}/articles`, { headers: { Authorization: `Bearer ${token}` } })
@@ -40,12 +79,31 @@ function userArticlesRequest() {
     })
 }
 
+function uploadImageRequest(formData) {
+  axios
+    .post(`${USER_API}/upload`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then((response) => {
+      const data = response.data
+      console.log(data)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
+
+// 渲染會員資訊
 function renderProfileInfo() {
-  avatar.src = user.avatar
+  avatarImg.src = user.avatar
   username.textContent = user.username
   email.textContent = user.email
 }
 
+// 渲染會員文章
 function renderUserArticles() {
   let htmlContent = ''
   articles.forEach((article) => {
