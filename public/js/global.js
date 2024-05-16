70f588d6bae47b8f39f84fbac6c86cc2db65d6e4
en <emily11737@gmail.com>
2024-05-16 13:16:47 +0800

    second revision - Dark mode icon switch & Home button

--- public/js/global.js
+++ public/js/global.js
@@ -1,4 +1,4 @@
-let loginState = 'Sign_out'
+let loginState = 'Sign_in'
 
 
 
@@ -16,6 +16,14 @@ if (loginState === 'Sign_out') {
   }
 
 
+
+const mode = document.querySelector('.darkmode')
+mode.addEventListener("click", function (event) {
+  let target = event.target;
+  target.classList.toggle("fa-sun"); // 切換 fa-sun 類名
+  target.classList.toggle("fa-moon"); // 切換 fa-moon 類名
+  
+})
 
 
 
