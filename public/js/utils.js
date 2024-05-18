ed5f2f6b1b1bc1781e705358ec9f9bbeb9bc7e1c
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-05-18 08:38:58 +0800

    utils: cookie

--- public/js/utils.js
+++ public/js/utils.js
@@ -14,3 +14,45 @@ class Storage {
 
 // 宣告Storage實例
 const storage = new Storage()
+
+let data = { articleId: 1, categoryId: 1 }
+document.cookie = `data=${JSON.stringify(data)}; secure=true; SameSite=none; path=/`
+
+// // Cookie
+// class Cookie {
+//   constructor() {
+//     // cookie名稱
+//     this.name = 'data'
+//     // 同步當前內容
+//     this.obj = this.get(this.name) || {}
+//     // cookie參數
+//     this.options = 'secure=true; samesite=none; path=/'
+//   }
+
+//   // 儲存
+//   set(key, value) {
+//     key === this.name ? (this.obj = value) : (this.obj[key] = value)
+//     document.cookie = `${this.name}=${JSON.stringify(this.obj)}; ${this.options}`
+//   }
+
+//   // 取得
+//   get(key) {
+//     const data = document.cookie
+//     const value = data.split('=')[1]
+//     if (value) {
+//       const parsedValue = JSON.parse(value)
+//       return key === this.name ? parsedValue : parsedValue[key]
+//     } else {
+//       return undefined
+//     }
+//   }
+
+//   // 刪除
+//   remove(key) {
+//     delete this.obj[key]
+//     this.set(this.name, this.obj)
+//   }
+// }
+
+// // 宣告Cookie實例
+// const cookie = new Cookie()
