38853ca68eba7bfa41cbb9060871a50b3aaf0b00
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-05-19 11:17:46 +0800

    adjust js structure

--- public/js/global.js
+++ public/js/global.js
@@ -1,22 +1,36 @@
-let loginState = 'Sign_out'
-
+// HTML元素
 const signIn = document.querySelector('.sign-in')
 const signUp = document.querySelector('.sign-up')
 const guest = document.querySelector('.guest')
-
-if (loginState === 'Sign_out') {
-  signIn.style.display = 'block'
-  signUp.style.display = 'block'
-  guest.style.display = 'none'
-} else if (loginState === 'Sign_in') {
-  signIn.style.display = 'none'
-  signUp.style.display = 'none'
-  guest.style.display = 'block'
-}
-
 const mode = document.querySelector('.darkmode')
-mode.addEventListener('click', function (event) {
+
+// 變數
+let loginState = 'Sign_out'
+
+// 初始函式
+;(function init() {
+  // 切換登入/登出樣式
+  toggleLoginView()
+  // 切換暗黑模式圖示
+  mode.addEventListener('click', onDarkMode)
+})()
+
+// 監聽器函式: 切換暗黑模式圖示
+function onDarkMode(event) {
   let target = event.target
   target.classList.toggle('fa-sun')
   target.classList.toggle('fa-moon')
-})
+}
+
+// 其他函式: 切換登入/登出樣式
+function toggleLoginView() {
+  if (loginState === 'Sign_out') {
+    signIn.style.display = 'block'
+    signUp.style.display = 'block'
+    guest.style.display = 'none'
+  } else if (loginState === 'Sign_in') {
+    signIn.style.display = 'none'
+    signUp.style.display = 'none'
+    guest.style.display = 'block'
+  }
+}
--- public/js/utils.js
+++ public/js/utils.js
@@ -1,3 +1,6 @@
+// API
+const BASE_URL = 'http://localhost:5000/v1'
+
 // Local Storage
 class Storage {
   // 儲存
@@ -15,44 +18,41 @@ class Storage {
 // 宣告Storage實例
 const storage = new Storage()
 
-let data = { articleId: 1, categoryId: 1 }
-document.cookie = `data=${JSON.stringify(data)}; secure=true; SameSite=none; path=/`
+// Cookie
+class Cookie {
+  constructor() {
+    // cookie名稱
+    this.name = 'data'
+    // 同步當前內容
+    this.obj = this.get(this.name) || {}
+    // cookie參數
+    this.options = 'secure=true; samesite=none; path=/'
+  }
 
-// // Cookie
-// class Cookie {
-//   constructor() {
-//     // cookie名稱
-//     this.name = 'data'
-//     // 同步當前內容
-//     this.obj = this.get(this.name) || {}
-//     // cookie參數
-//     this.options = 'secure=true; samesite=none; path=/'
-//   }
+  // 儲存
+  set(key, value) {
+    key === this.name ? (this.obj = value) : (this.obj[key] = value)
+    document.cookie = `${this.name}=${JSON.stringify(this.obj)}; ${this.options}`
+  }
 
-//   // 儲存
-//   set(key, value) {
-//     key === this.name ? (this.obj = value) : (this.obj[key] = value)
-//     document.cookie = `${this.name}=${JSON.stringify(this.obj)}; ${this.options}`
-//   }
+  // 取得
+  get(key) {
+    const data = document.cookie
+    const value = data.split('=')[1]
+    if (value) {
+      const parsedValue = JSON.parse(value)
+      return key === this.name ? parsedValue : parsedValue[key]
+    } else {
+      return undefined
+    }
+  }
 
-//   // 取得
-//   get(key) {
-//     const data = document.cookie
-//     const value = data.split('=')[1]
-//     if (value) {
-//       const parsedValue = JSON.parse(value)
-//       return key === this.name ? parsedValue : parsedValue[key]
-//     } else {
-//       return undefined
-//     }
-//   }
+  // 刪除
+  remove(key) {
+    delete this.obj[key]
+    this.set(this.name, this.obj)
+  }
+}
 
-//   // 刪除
-//   remove(key) {
-//     delete this.obj[key]
-//     this.set(this.name, this.obj)
-//   }
-// }
-
-// // 宣告Cookie實例
-// const cookie = new Cookie()
+// 宣告Cookie實例
+const cookie = new Cookie()
