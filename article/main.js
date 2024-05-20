e45b316c5c75fa73afeeaad9dc037fadf0a68551
Elva Su <112391401+HWenSu@users.noreply.github.com>
2024-05-20 20:26:29 +0800

    Update: textarea animation

--- article/main.js
+++ article/main.js
@@ -1,15 +1,44 @@
 // 登入功能還未完成, 先以點擊按鈕後就出現可以留言示意
 
-// listen to sign-in btn
+const body = document.querySelector('body')
 const commentArea = document.querySelector('.comment-area')
 
+
+
+// listen to sign-in btn
 signIn.addEventListener('click', (e)=> {
   target = e.target
   commentArea.innerHTML = `
-    <textarea name="user-comment" id="user-comment"></textarea>
+    <textarea name="user-comment" id="user-comment" class="text-area"></textarea>
     <div class="comment-btn-container">
-      <button class="comment-cancel">取消</button>
-      <input type="submit" value="確定">
     </div>
   `
 })
+
+//listen to body
+body.addEventListener('click', (e)=> {
+  target = e.target
+  const commentBtn = document.querySelector('.comment-btn-container')
+  const textArea = document.querySelector('#user-comment')
+  // let isCommented = false
+
+  if (target.tagName === 'TEXTAREA') {
+    commentBtn.innerHTML = `
+      <button class="comment-cancel">取消</button>
+      <input type="submit" value="確定">
+    `
+    textArea.classList.add('textarea-clicked')
+  }
+  
+  // text area without content, reset height
+  if(target.tagName !== 'TEXTAREA' && textArea.value.length === 0) {
+      textArea.classList.remove('textarea-clicked')
+      commentBtn.innerHTML = ''
+  } else if ( target.classList.contains('comment-cancel') ){
+      textArea.value = ''
+      textArea.classList.remove('textarea-clicked')
+      commentBtn.innerHTML = ''
+  }
+})
+
+
