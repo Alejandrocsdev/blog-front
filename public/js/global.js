b69aeb77cb2d54e1ecc18647173591760b991934
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-05-20 22:01:49 +0800

    restructure global.js darkmode

--- public/js/global.js
+++ public/js/global.js
@@ -1,49 +1,62 @@
-let loginState = 'Sign_out'
-
+// HTML元素
 const signIn = document.querySelector('.sign-in')
 const signUp = document.querySelector('.sign-up')
 const guest = document.querySelector('.guest')
-
 const mode = document.querySelector('.darkmode')
-const body = document.querySelector('body')
+const body = document.body
 
-if (loginState === 'Sign_out') {
-  signIn.style.display = 'block'
-  signUp.style.display = 'block'
-  guest.style.display = 'none'
-} else if (loginState === 'Sign_in') {
-  signIn.style.display = 'none'
-  signUp.style.display = 'none'
-  guest.style.display = 'block'
-}
+// 變數
+let loginState = 'Sign_out'
 
-//init function
+// 初始函式
 ;(function init() {
-  if( storage.get("theme") === 'dark' ) {
-    switchModeIcon ()
-    body.classList.add('dark')
-  }
+  // 設定暗黑模式狀態
+  setTheme()
+  // 切換登入/登出樣式
+  toggleLoginView()
+  // 監聽器: 切換暗黑模式
+  mode.addEventListener('click', onToggleMode)
 })()
 
-//listen to mode btn
-mode.addEventListener('click', function (event) {
-  let target = event.target
-  switchModeIcon ()
-  // dark mode style and set key to local storage
-  if ( target.classList.contains('fa-moon') ){  
-    body.classList.add('dark')
-    storage.set("theme", "dark")
+// 監聽器函式: 切換暗黑模式
+function onToggleMode(event) {
+  const target = event.target
+  toggleModeIcon()
+  if (target.classList.contains('fa-moon')) {
+    storage.set('theme', 'dark')
   } else {
-    body.classList.remove('dark')
-    storage.remove("theme")
+    storage.set('theme', 'light')
   }
-})
-
-//switch mode icon function
-function switchModeIcon () {
-  mode.classList.toggle('fa-sun')
-  mode.classList.toggle('fa-moon')
 }
 
+// 其他函式: 切換暗黑模式圖示
+function toggleModeIcon() {
+  // 切換圖示
+  mode.classList.toggle('fa-sun')
+  mode.classList.toggle('fa-moon')
+  // 切換主題
+  body.classList.toggle('dark')
+}
 
+// 其他函式: 設定暗黑模式狀態
+function setTheme() {
+  if (!storage.get('theme')) {
+    storage.set('theme', 'light')
+  } else if (storage.get('theme') === 'dark') {
+    toggleModeIcon()
+    storage.set('theme', 'dark')
+  }
+}
 
+// 其他函式: 切換登入/登出樣式
+function toggleLoginView() {
+  if (loginState === 'Sign_out') {
+    signIn.style.display = 'block'
+    signUp.style.display = 'block'
+    guest.style.display = 'none'
+  } else if (loginState === 'Sign_in') {
+    signIn.style.display = 'none'
+    signUp.style.display = 'none'
+    guest.style.display = 'block'
+  }
+}
--- public/js/utils.js
+++ public/js/utils.js
@@ -1,3 +1,6 @@
+// 起始網址
+const BASE_URL = 'http://localhost:5000/v1'
+
 // Local Storage
 class Storage {
   // 儲存
@@ -10,54 +13,46 @@ class Storage {
     const value = JSON.parse(localStorage.getItem(key))
     return value
   }
+}
 
-  //刪除
-  remove(key){
-    localStorage.removeItem(key)
+// 宣告實例
+const storage = new Storage()
+
+// Cookie
+class Cookie {
+  constructor() {
+    // cookie名稱
+    this.name = 'data'
+    // 同步當前內容
+    this.obj = this.get(this.name) || {}
+    // cookie參數
+    this.options = 'secure=true; samesite=none; path=/'
+  }
+
+  // 儲存
+  set(key, value) {
+    key === this.name ? (this.obj = value) : (this.obj[key] = value)
+    document.cookie = `${this.name}=${JSON.stringify(this.obj)}; ${this.options}`
+  }
+
+  // 取得
+  get(key) {
+    const data = document.cookie
+    const value = data.split('=')[1]
+    if (value) {
+      const parsedValue = JSON.parse(value)
+      return key === this.name ? parsedValue : parsedValue[key]
+    } else {
+      return undefined
+    }
+  }
+
+  // 刪除
+  remove(key) {
+    delete this.obj[key]
+    this.set(this.name, this.obj)
   }
 }
 
-// 宣告Storage實例
-const storage = new Storage()
-
-let data = { articleId: 1, categoryId: 1 }
-document.cookie = `data=${JSON.stringify(data)}; secure=true; SameSite=none; path=/`
-
-// // Cookie
-// class Cookie {
-//   constructor() {
-//     // cookie名稱
-//     this.name = 'data'
-//     // 同步當前內容
-//     this.obj = this.get(this.name) || {}
-//     // cookie參數
-//     this.options = 'secure=true; samesite=none; path=/'
-//   }
-
-//   // 儲存
-//   set(key, value) {
-//     key === this.name ? (this.obj = value) : (this.obj[key] = value)
-//     document.cookie = `${this.name}=${JSON.stringify(this.obj)}; ${this.options}`
-//   }
-
-//   // 取得
-//   get(key) {
-//     const data = document.cookie
-//     const value = data.split('=')[1]
-//     if (value) {
-//       const parsedValue = JSON.parse(value)
-//       return key === this.name ? parsedValue : parsedValue[key]
-//     } else {
-//       return undefined
-//     }
-//   }
-
-//   // 刪除
-//   remove(key) {
-//     delete this.obj[key]
-//     this.set(this.name, this.obj)
-//   }
-// }
-
-// // 宣告Cookie實例
-// const cookie = new Cookie()
+// 宣告實例
+const cookie = new Cookie()
