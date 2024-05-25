6dfe932fe54b6d9c9306df5529e27bd3d1ca60ec
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-05-25 19:04:27 +0800

    complete update

---
+++ article/main.js
@@ -0,0 +1,116 @@
+'use strict'
+
+// HTML元素
+// const logInComment = document.querySelector('.log-in-comment')
+// const logOutComment = document.querySelector('.log-out-comment')
+const commentBtn = document.querySelector('.comment-btn-container')
+const textArea = document.querySelector('#user-comment')
+const article = document.querySelector('article')
+
+// 留言區狀態
+let isTextareaActive = false
+
+const articleData = []
+
+// 索取cookie值
+const id = cookie.get('articleId')
+
+// 初始函式
+;(function init() {
+  getArticle()
+  // 監聽器: 登入狀態
+  // nav.addEventListener('click', onLoginState)
+  // 監聽器: 留言區
+  body.addEventListener('click', onTextarea)
+})()
+
+function getArticle() {
+  // 索取id頁面資料
+  axios
+    .get(BASE_URL + '/articles/' + id)
+    .then((response) => {
+      const data = response.data
+      console.log(data)
+      articleData.push(data)
+      renderArticle(articleData[0])
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+// 重新渲染頁面
+function renderArticle(articleData) {
+  let rawHTML = ``
+  const categories = articleData.categories
+
+  rawHTML += `<h2 class="title">${articleData.title}</h2>
+        <div class="user">
+          <img src="${articleData.picture}" alt="avatar" />
+          <a href="#" class="username"> ${articleData.user.username}</a>
+          <ul class="categories">`
+
+  rawHTML += renderCategories(categories)
+
+  rawHTML += `</ul>
+          </div>
+          <img class="article-img" src="${articleData.picture}"
+            alt="article-image" />
+          <div class="article-content">
+            ${articleData.content}
+          </div>`
+
+  article.innerHTML = rawHTML
+}
+
+function renderCategories(categories) {
+  let rawHTML = ''
+  categories.forEach((e) => {
+    rawHTML += `<li><a class="category" href="#">${e.category}</a></li>`
+  })
+  return rawHTML
+}
+
+// 監聽器函式: 登入狀態
+// function onLoginState(event) {
+//   const target = event.target
+//   if (target.classList.contains('sign-in')) {
+//     isLoggedIn = true
+//     switchCommentArea()
+//   } else if (target.classList.contains('sign-up')) {
+//     isLoggedIn = false
+//     switchCommentArea()
+//   }
+// }
+
+//監聽器函式: 留言區狀態
+function onTextarea(event) {
+  const target = event.target
+
+  if (target.tagName === 'TEXTAREA' && !isTextareaActive) {
+    isTextareaActive = true
+    textArea.classList.add('textarea-clicked')
+    commentBtn.classList.remove('hidden')
+  }
+
+  if (target.tagName !== 'TEXTAREA' && !textArea.value) {
+    isTextareaActive = false
+    textArea.classList.remove('textarea-clicked')
+    commentBtn.classList.add('hidden')
+  } else if (target.classList.contains('comment-cancel')) {
+    commentBtn.classList.add('hidden')
+    textArea.value = ''
+    textArea.classList.remove('textarea-clicked')
+  }
+}
+
+// 函式: 留言區樣式轉換
+// function switchCommentArea() {
+//   if (isLoggedIn) {
+//     logInComment.classList.remove('hidden')
+//     logOutComment.classList.add('hidden')
+//   } else {
+//     logInComment.classList.add('hidden')
+//     logOutComment.classList.remove('hidden')
+//   }
+// }
--- public/js/global.js
+++ public/js/global.js
@@ -1,12 +1,20 @@
+'use strict'
+
 // HTML元素
+const nav = document.querySelector('#navigation')
 const signIn = document.querySelector('.sign-in')
 const signUp = document.querySelector('.sign-up')
 const guest = document.querySelector('.guest')
 const mode = document.querySelector('.darkmode')
+const body = document.body
+const modalBg = document.querySelector('.modal-bg')
+const logInComment = document.querySelector('.log-in-comment')
+const logOutComment = document.querySelector('.log-out-comment')
+const loginSwitch = document.getElementById('login-switch')
 
 // 變數
-let loginState = 'Sign_out'
-let isModalOpen = false; // 初始 Modal為關閉
+let isLoggedIn = cookie.get('isLoggedIn') || false
+console.log('isLoggedIn: ', isLoggedIn)
 
 // 初始函式
 ;(function init() {
@@ -14,25 +22,9 @@ let isModalOpen = false; // 初始 Modal為關閉
   toggleLoginView()
   // 切換暗黑模式圖示
   mode.addEventListener('click', onDarkMode)
+  // 切換登入狀態
+  loginSwitch.addEventListener('click', onSwitchState)
 })()
-
-//點擊signIn時，建立並顯示signInModal
-signIn.addEventListener("click", function () {
-  if (!isModalOpen) {
-    isModalOpen = true; 
-    const signInModal = createSignInModal();
-    showSignInModal(signInModal, signIn);
-  }
-});
-
-// 點擊signUp時，建立並顯示signUpModal
-signUp.addEventListener("click", function () {
-  if (!isModalOpen) {
-    isModalOpen = true;
-    const signUpModal = createSignUpModal(); 
-    showSignUpModal(signUpModal, signUp);
-  }
-});
 
 // 監聽器函式: 切換暗黑模式圖示
 function onDarkMode(event) {
@@ -43,119 +35,130 @@ function onDarkMode(event) {
 
 // 其他函式: 切換登入/登出樣式
 function toggleLoginView() {
-  if (loginState === 'Sign_out') {
-    signIn.style.display = 'block'
-    signUp.style.display = 'block'
-    guest.style.display = 'none'
-  } else if (loginState === 'Sign_in') {
+  if (isLoggedIn) {
     signIn.style.display = 'none'
     signUp.style.display = 'none'
     guest.style.display = 'block'
+  } else {
+    signIn.style.display = 'block'
+    signUp.style.display = 'block'
+    guest.style.display = 'none'
   }
 }
 
-// 建立signInModal
-function createSignInModal() {  
-  const signInModal = document.createElement("div");
-  signInModal.id = "signInModal";
-  signInModal.className = "sign-in-modal-content";
-  signInModal.innerHTML = `
-    <div>
-      <button class="sign-in-close">X</button>
-    </div>
-    <h3 class="sign-up">登入</h3>      
-    <div class="sign-in-content-table">
-      <div class="content">
-        <div class="title">帳號</div>
-        <div class="colon">:</div>
-        <input type="text">
-      </div>
-      <div class="content">
-        <div class="title">密碼</div>
-        <div class="colon">:</div>
-        <input type="password">
-      </div>
-    </div>    
-    <div class="submit">
-      <button>提交</button>
-    </div>
-  `;
-
-  document.body.appendChild(signInModal);
-  return signInModal;
+function setLoginState() {
+  isLoggedIn ? (loginSwitch.textContent = '登入') : (loginSwitch.textContent = '登出')
+  cookie.set('isLoggedIn', isLoggedIn)
+  console.log(isLoggedIn)
 }
 
-// 顯示signInModal
-function showSignInModal(signInModal) {
-  signInModal.style.display = "block";
+function onSwitchState() {
+  if (isLoggedIn) {
+    loginSwitch.textContent = '登出'
+    isLoggedIn = false
+  } else {
+    loginSwitch.textContent = '登入'
+    isLoggedIn = true
+  }
+  updateState()
+  cookie.set('isLoggedIn', isLoggedIn)
+  console.log(isLoggedIn)
+}
 
-  // signInModal顯示時的背景
-  const modalOverlay = document.createElement("div");
-  modalOverlay.className = "modal-overlay";
-  document.body.appendChild(modalOverlay);
+// 更新登入狀態
+function updateState() {
+  const pathname = window.location.pathname
+  if (pathname === '/article/index.html') {
+    switchCommentArea()
+  } 
+}
 
-  // sign-in-close監聽器(顯示signInModal時才有)
-  const closeButton = signInModal.querySelector(".sign-in-close");
-  closeButton.addEventListener("click", function () {
-    signInModal.style.display = "none";
-    modalOverlay.remove(); // 移除signInModal顯示時的背景
-    isModalOpen = false; // modal回初始
-  }); 
+function switchCommentArea() {
+  if (isLoggedIn) {
+    logInComment.classList.add('hidden')
+    logOutComment.classList.remove('hidden')
+  } else {
+    logInComment.classList.remove('hidden')
+    logOutComment.classList.add('hidden')
+  }
+}
+
+//點擊signIn時，建立並顯示signInModal
+signIn.addEventListener('click', function () {
+  modalBg.innerHTML = createSignInModal()
+  modalBg.classList.toggle('hidden')
+  const modalClose = modalBg.querySelector('.modal-close')
+  modalClose.addEventListener('click', onModalClose)
+})
+
+// 點擊signUp時，建立並顯示signUpModal
+signUp.addEventListener('click', function () {
+  modalBg.innerHTML = createSignUpModal()
+  modalBg.classList.toggle('hidden')
+  const modalClose = modalBg.querySelector('.modal-close')
+  modalClose.addEventListener('click', onModalClose)
+})
+
+// 建立signInModal
+function createSignInModal() {
+  return `<div id="signInModal" class="sign-in-modal-content">
+  <div>
+    <button class="modal-close">X</button>
+  </div>
+  <h3 class="sign-up">登入</h3>      
+  <div class="sign-in-content-table">
+    <div class="content">
+      <div class="title">帳號</div>
+      <div class="colon">:</div>
+      <input type="text">
+    </div>
+    <div class="content">
+      <div class="title">密碼</div>
+      <div class="colon">:</div>
+      <input type="password">
+    </div>
+  </div>    
+  <div class="submit">
+    <button>提交</button>
+  </div>
+</div>`
 }
 
 // 建立signUpModal
 function createSignUpModal() {
-  const signUpModal = document.createElement("div");
-  signUpModal.id = "signUpModal"; 
-  signUpModal.className = "sign-up-modal-content";
-  signUpModal.innerHTML = `
-    <div>
-      <button class="sign-up-close">X</button>
+  return `<div id="signUpModal" class="sign-up-modal-content">
+  <div>
+    <button class="modal-close">X</button>
+  </div>
+  <h3 class="sign-up">註冊</h3>
+  <div class="sign-up-content-table">
+    <div class="content">
+      <div class="title">帳號</div>
+      <div class="colon">:</div>
+      <input type="text">
     </div>
-    <h3 class="sign-up">註冊</h3>
-    <div class="sign-up-content-table">
-      <div class="content">
-        <div class="title">帳號</div>
-        <div class="colon">:</div>
-        <input type="text">
-      </div>
-      <div class="content">
-        <div class="title">信箱</div>
-        <div class="colon">:</div>
-        <input type="text">
-      </div>
-      <div class="content">
-        <div class="title">密碼</div>
-        <div class="colon">:</div>
-        <input type="password">
-      </div>
-      <div class="content">
-        <div class="title">確認密碼</div>
-        <div class="colon">:</div>
-        <input type="password">
-      </div>
+    <div class="content">
+      <div class="title">信箱</div>
+      <div class="colon">:</div>
+      <input type="text">
     </div>
-    <div class="submit">
-      <button>提交</button>
+    <div class="content">
+      <div class="title">密碼</div>
+      <div class="colon">:</div>
+      <input type="password">
     </div>
-  `;
-
-  document.body.appendChild(signUpModal);
-  return signUpModal;
+    <div class="content">
+      <div class="title">確認密碼</div>
+      <div class="colon">:</div>
+      <input type="password">
+    </div>
+  </div>
+  <div class="submit">
+    <button>提交</button>
+  </div>
+</div>`
 }
 
-// 顯示 signUpModal
-function showSignUpModal(signUpModal) {
-  signUpModal.style.display = "block";
-
-  const modalOverlay = document.createElement("div");
-  modalOverlay.className = "modal-overlay";
-  document.body.appendChild(modalOverlay);
-
-  const closeButton = signUpModal.querySelector(".sign-up-close");
-  closeButton.addEventListener("click", function () {
-    signUpModal.style.display = "none";
-    modalOverlay.remove();
-    isModalOpen = false; // modal回初始
-  });
+function onModalClose() {
+  modalBg.classList.toggle('hidden')
 }
