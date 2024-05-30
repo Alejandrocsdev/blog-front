508cf7320020ea2c3afdbbaeda307fef9d8007fa
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-05-30 22:11:11 +0800

    slightly restructure article/main.js

--- article/main.js
+++ article/main.js
@@ -7,12 +7,12 @@ const ARTICLE_URL = `${BASE_URL}/articles`
 const commentBtn = document.querySelector('.comment-btn-container')
 const textArea = document.querySelector('.user-comment')
 const articleContainer = document.querySelector('article')
-const commentHistory = document.querySelector(".comment-history-container");
+const commentHistory = document.querySelector('.comment-history-container')
 
 // 儲存單篇文章
 const article = []
 // 處存歷史留言
-const comments= []
+const comments = []
 
 // 留言區狀態
 let isTextareaActive = false
@@ -26,7 +26,7 @@ console.log('文章ID: ', articleId)
   // 取得文章資料
   getArticle()
   // 取得留言資料
-  getComment();
+  getComments()
   // 監聽器: 留言區
   body.addEventListener('click', onTextarea)
 })()
@@ -38,8 +38,8 @@ function getArticle() {
     .then((response) => {
       const data = response.data
       article.push(data)
-      console.log('回傳資料: ', data)
-      console.log('儲存資料: ', article)
+      console.log('回傳文章資料: ', data)
+      console.log('儲存文章資料: ', article)
       // 渲染單篇文章
       renderArticle(article[0])
     })
@@ -49,21 +49,23 @@ function getArticle() {
 }
 
 // API: 取得留言資料
-function getComment() {
-    axios
-      .get(`${BASE_URL}/comments/${articleId}`)
-      .then((response) => {
-        const data = response.data.main
-        comments.push(data);
-        console.log('回傳留言資料: ', data);
-        console.log('儲存留言資料: ', comments);
-        // 渲染留言
-        renderComments(comments);
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-} 
+function getComments() {
+  axios
+    .get(`${BASE_URL}/comments/${articleId}`)
+    .then((response) => {
+      const data = response.data
+      const main = data.main
+      comments.push(...main)
+      console.log('回傳留言資料: ', data)
+      console.log('主體留言資料: ', main)
+      console.log('儲存留言資料: ', comments)
+      // 渲染留言
+      renderComments(comments)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
 
 // 渲染單篇文章
 function renderArticle(article) {
@@ -95,15 +97,15 @@ function renderCategories(categories) {
 // 渲染歷史留言
 function renderComments(comments) {
   let rawHTML = ''
-  comments[0].forEach((commentInfo)=> {
-      rawHTML += `
-      <div class="comment-container ${commentInfo.id}">
-          <div class="user-info ${commentInfo.user.id}">
-            <img class="comment-avatar" src="${commentInfo.user.avatar}">
-            <div class="comment-username">${commentInfo.user.username}</div>
+  comments.forEach((e) => {
+    rawHTML += `
+      <div class="comment-container ${e.id}">
+          <div class="user-info ${e.user.id}">
+            <img class="comment-avatar" src="${e.user.avatar}">
+            <div class="comment-username">${e.user.username}</div>
           </div>
          <div class="comment-area">
-          <span>${commentInfo.comment}</span>
+          <span>${e.comment}</span>
         </div>
       </div>`
   })
