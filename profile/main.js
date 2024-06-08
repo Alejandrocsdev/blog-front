63bd0349cf7a3bf241f1815dc491f90a94590863
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-06-08 20:59:31 +0800

    merge task 32 into develop

--- profile/main.js
+++ profile/main.js
@@ -1,14 +1,30 @@
 'use strict'
 
+// API
+const ARTICLES_URL = `${BASE_URL}/articles`
+
 // HTML 元素
 const userArticles = document.querySelector('.user-articles')
+const userAvatar = document.querySelector('.user-avatar img')
+const profileAvatar = document.querySelector('.profile-avatar img')
+const fileUploader = document.querySelector('#file-uploader')
+const account = document.querySelector('.user-account')
+const email = document.querySelector('.user-email')
 
 const articles = []
 
+let id = user.id
+console.log(user)
+
 // 初始函式
 ;(function init() {
+  getUserdata(user)
   // 取得文章資料
   getUserArticles()
+
+  fileUploader.addEventListener('change', onSubmit)
+
+  userAvatar.addEventListener('click', () => fileUploader.click())
 })()
 
 // API: 取得用戶文章資料
@@ -35,4 +51,49 @@ function renderUserArticles(articles) {
     rawHTML += `<li><a href="../edit/index.html">${article.title}</a></li>`
   })
   userArticles.innerHTML = rawHTML
+}
+
+// 更新user資料
+function getUserdata(user) {
+  //頭像設置
+  userAvatar.src = user.avatar
+  profileAvatar.src = user.avatar
+  //會員資料設置
+  account.textContent = user.username
+  email.textContent = user.email
+}
+
+//上傳頭像至後端
+function onSubmit(event) {
+  if (event) {
+    event.preventDefault()
+  }
+  const file = fileUploader.files[0]
+  if (file) {
+    let formData = new FormData()
+    formData.append('file', file)
+
+    axios
+      .patch(`${BASE_URL}/users/${id}/upload`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      .then((data) => {
+        user = data.data.user
+        console.log('Success:', data)
+        alert('圖片上傳成功!')
+        cookie.set('user', data.data.user)
+        console.log('已更新cookie')
+        getUserdata(user)
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+        alert('圖片上傳失敗')
+      })
+  } else {
+    alert('請選擇圖片')
+  }
 }
