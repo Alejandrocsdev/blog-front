3c3fb98a8d057864176cd1c802bb4f913eb7f606
Amber Wang <wangshengyan6@gmail.com>
2024-06-15 14:00:42 +0800

    add delete article api

--- edit/main.js
+++ edit/main.js
@@ -1,1 +1,23 @@
-'use strict'
+"use strict";
+// 取得刪除文章按鈕(還沒有刪除按鈕)
+const deleteArticleBtn = document.querySelector(".deleteBtn");
+// 監聽刪除文章按鈕
+deleteArticleBtn.addEventListener("click", onDeleteArticle);
+// 刪除文章函式
+function onDeleteArticle(articleId, token) {
+  axios
+    .delete(`${BASE_URL}/articles/${articleId}/delete`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      console.log(response.data.message);
+      // 重新渲染頁面頁面
+      getArticles();
+    })
+    .catch((error) => {
+      alert("發生錯誤，請再試一次");
+      console.log(error);
+    });
+}
